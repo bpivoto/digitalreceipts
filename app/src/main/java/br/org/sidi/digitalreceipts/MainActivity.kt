8d19799e224b43a308f455b43a4302c5a5bff912
Bruno Pivoto Azevedo <bpivoto@gmail.com>
2022-08-07 22:45:42 -0300

    Initial commit

---
+++ app/src/main/java/br/org/sidi/digitalreceipts/MainActivity.kt
@@ -0,0 +1,92 @@
+package br.org.sidi.digitalreceipts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import br.org.sidi.digitalreceipts.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private val TAG = "[MainActivity]"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = Firebase.auth
+
+        // init googleSignInClient
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("635335334034-tmiqipvma38pvau2m25jmgrn3m8phvpt.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        binding.btnGoogleLogin.setOnClickListener {
+            signInGoogle()
+        }
+    }
+
+    private fun signInGoogle() {
+        val intent = googleSignInClient.signInIntent
+        openAct.launch(intent)
+    }
+
+    var openAct = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        result: ActivityResult ->
+        Log.i(TAG, "ResultCode=" + result.resultCode.toString())
+        if (result.resultCode == RESULT_OK) {
+            val intent = result.data
+            val task = GoogleSignIn.getSignedInAccountFromIntent(intent)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                loginWithGoogle(account.idToken)
+            } catch (exception: ApiException) {
+                Log.e(TAG, exception.toString())
+            }
+        }
+    }
+
+    private fun loginWithGoogle(token: String?) {
+        val credential = GoogleAuthProvider.getCredential(token, null)
+        auth.signInWithCredential(credential).addOnCompleteListener(this) {
+            task: Task<AuthResult> ->
+            if (task.isSuccessful) {
+                Toast.makeText(baseContext, "Authentication OK (Google)",
+                    Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(baseContext, "Authentication failed! (Google)",
+                    Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        var currentUser = auth.getCurrentUser()
+        //TODO
+        //updateUI(currentUser);
+    }
+
+
+}
